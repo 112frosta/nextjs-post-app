257af31a319515ab1066f1cf3e3dfb7d06311af6
112frosta <112frosta@proton.me>
2024-02-18 23:29:16 +0100

    Add disabled styles to Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,7 @@ export default function Button(props: ButtonProps) {
     <button
       {...props}
       className={twMerge(
-        "bg-blue-700 text-white px-4 py-2.5 rounded-lg font-medium hover:bg-blue-800 transition-colors duration-150",
+        "bg-blue-700 text-white disabled:bg-blue-900 disabled:hover:bg-blue-900 px-4 py-2.5 rounded-lg font-medium hover:bg-blue-800 transition-colors duration-150",
         props?.className
       )}
     >
